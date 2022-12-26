73f481f6d9c8c310442aa34de3abf8eb43e453d2
blemes319 <bruno.santos@climatempo.io>
2022-12-25 22:44:44 -0300

    feat: aula 01

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
+class MaquinaDeSuco {
+  String typeFruit;
+  MaquinaDeSuco({this.typeFruit = '--'});
+  get getTypeFruit => typeFruit;
+
+  void queSucoEstaSendoProduzido() {
+    print('O suco que está sendo produzido é: ' + typeFruit);
+  }
+}
+
 void main(List<String> args) {
-  print('teste');
+  MaquinaDeSuco maquina = MaquinaDeSuco(typeFruit: 'Manga');
+  maquina.queSucoEstaSendoProduzido();
 }
